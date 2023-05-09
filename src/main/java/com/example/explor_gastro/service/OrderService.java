626935e0733f8c57ddedc302fde6f43f6b669261
20406 <2040644775@qq.com>
2023-05-09 08:58:48 +0800

    订单

---
+++ src/main/java/com/example/explor_gastro/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.example.explor_gastro.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.explor_gastro.entity.Order;
+
+/**
+ * 订单表(Order)表服务接口
+ *
+ * @author makejava
+ * @since 2023-05-09 08:53:40
+ */
+public interface OrderService extends IService<Order> {
+
+}
+
