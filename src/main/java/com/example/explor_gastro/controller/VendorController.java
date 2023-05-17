e682f623bc337873cef2fa416a4469e630d0ac63
20406 <2040644775@qq.com>
2023-05-17 11:43:39 +0800

    商家分页查询和修改

--- src/main/java/com/example/explor_gastro/controller/VendorController.java
+++ src/main/java/com/example/explor_gastro/controller/VendorController.java
@@ -2,9 +2,12 @@ package com.example.explor_gastro.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.api.ApiController;
 import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.explor_gastro.entity.Product;
+import com.example.explor_gastro.entity.Star;
 import com.example.explor_gastro.entity.User;
 import com.example.explor_gastro.entity.Vendor;
 import com.example.explor_gastro.service.VendorService;
@@ -42,35 +45,38 @@ public class VendorController extends ApiController {
 
 
     /**
-     * 分页查询所有数据
+     * 分页查询所有商家
      *
      * @param page   分页对象
      * @param vendor 查询实体
      * @return 所有数据
      */
     @GetMapping("vendorInquire")
-    @Operation(summary = "所有商家")
+    @Operation(summary = "分页查询所有商家")
     @Parameters({
-            @Parameter(name = "page", description = "分页对象"),
-            @Parameter(name = "vendor", description = "查询实体"),
+            @Parameter(name = "page", description = "查询的页面"),
+            @Parameter(name = "vendor", description = "每页显示多少商家"),
+            @Parameter(name = "isAsc", description = "是否升序排列 false(否) true(是)"),
+            @Parameter(name = "sortField", description = "根据此参数传入的字段排序")
     })
-    public R selectAll(
-            @RequestParam(name = "page",defaultValue = "1") int page1,
-            @RequestParam(name = "vendor",defaultValue = "1") int vendor1,
-            Page<Vendor> page, Vendor vendor) {
-        return success(this.vendorService.page(page, new QueryWrapper<>(vendor)));
+    public IPage<Vendor> page(
+            @RequestParam(name = "page",defaultValue = "1") int page,
+            @RequestParam(name = "vendor",defaultValue = "10") int vendor,
+            @RequestParam(name = "isAsc", required = false,defaultValue = "true") Optional<Boolean> isAsc,
+            @RequestParam(name = "sortField", required = false,defaultValue = "vendor_id") Optional<String> sortField) {
+        return vendorService.testSelectPage(page, vendor, isAsc, sortField);
     }
 
     /**
      * 通过主键查询单条数据
      *
-     * @param id 主键
+     * @param id 商家id
      * @return 单条数据
      */
     @PostMapping("/{id}/vendorid")
-    @Operation(summary = "商家详细")
+    @Operation(summary = "查看商家信息")
     @Parameters({
-            @Parameter(name = "id", description = "主键"),
+            @Parameter(name = "id", description = "商家id"),
     })
     public R selectOne(
             @RequestParam(name = "id",defaultValue = "1") int id1,
@@ -84,13 +90,13 @@ public class VendorController extends ApiController {
          * 商家登录接口
          *
          * phone 手机号
-         * password 用户密码
+         * password 商家密码
          */
         @PostMapping(value = "/login",produces  =  "text/plain;charset=UTF-8")
         @Operation(summary = "商家登录")
         @Parameters({
                 @Parameter(name = "phone", description = "手机号"),
-                @Parameter(name = "password", description = "用户密码"),
+                @Parameter(name = "password", description = "商家密码"),
         })
         public String login(@RequestParam(defaultValue = "12222222222") String phone, @RequestParam(defaultValue = "5555") String password){
             Vendor vendor=vendorService.LoginIn(phone, password);
@@ -103,17 +109,17 @@ public class VendorController extends ApiController {
         /**
          * 商家注册接口
          *
-         * username 用户名
+         * username 商家名
          * phone 手机号
-         * password 用户密码
+         * password 商家密码
          *
          */
         @PostMapping(value = "/register",produces  =  "text/plain;charset=UTF-8")
         @Operation(summary = "商家注册")
         @Parameters({
-                @Parameter(name = "username", description = "用户名"),
+                @Parameter(name = "username", description = "商家名"),
                 @Parameter(name = "phone", description = "手机号"),
-                @Parameter(name = "password", description = "用户密码"),
+                @Parameter(name = "password", description = "商家密码"),
         })
         public String register(@RequestParam String username, @RequestParam String phone, @RequestParam String password) {
             Vendor vendor=vendorService.register(username, phone, password);
@@ -122,7 +128,24 @@ public class VendorController extends ApiController {
             }
             return "注册成功";
         }
+
+    /**
+     * 修改数据
+     *
+     * @param vendor 实体对象
+     * @return 修改结果
+     */
+    @PutMapping("update")
+    @Operation(summary = "修改商家信息")
+    @Parameters({
+            @Parameter(name = "vendor", description = "修改商家信息"),
+    })
+    public R update(@RequestBody Vendor vendor) {
+        return success(this.vendorService.updateById(vendor));
     }
+
+
+
 
 //    /**
 //     * 新增数据
@@ -138,22 +161,6 @@ public class VendorController extends ApiController {
 //    public R insert(@RequestBody Vendor vendor) {
 //        return success(this.vendorService.save(vendor));
 //    }
-
-//    /**
-//     * 修改数据
-//     *
-//     * @param vendor 实体对象
-//     * @return 修改结果
-//     */
-//    @PutMapping("update")
-//    @Operation(summary = "修改数据")
-//    @Parameters({
-//            @Parameter(name = "vendor", description = "实体对象"),
-//    })
-//    public R update(@RequestBody Vendor vendor) {
-//        return success(this.vendorService.updateById(vendor));
-//    }
-
 //    /**
 //     * 删除数据
 //     *
@@ -168,3 +175,4 @@ public class VendorController extends ApiController {
 //    public R delete(@RequestParam("idList") List<Long> idList) {
 //        return success(this.vendorService.removeByIds(idList));
 //    }
+}
--- src/main/java/com/example/explor_gastro/service/VendorService.java
+++ src/main/java/com/example/explor_gastro/service/VendorService.java
@@ -1,10 +1,16 @@
 package com.example.explor_gastro.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.explor_gastro.entity.Product;
+import com.example.explor_gastro.entity.Star;
 import com.example.explor_gastro.entity.User;
 import com.example.explor_gastro.entity.Vendor;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 
 /**
  * 商家表(Vendor)表服务接口
@@ -19,6 +25,7 @@ public interface VendorService extends IService<Vendor> {
 
     Vendor register(String name, String password,String phone);
 
+    public IPage<Vendor> testSelectPage(int current, int size, Optional<Boolean> isAsc, Optional<String> sortField);
 }
 
 
